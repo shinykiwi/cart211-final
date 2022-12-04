72830bc1b8ff6d3b49947c5692fb84dadf7921da
Kira <kafountain5@gmail.com>
2022-12-04 13:40:19 -0500

    music implementation

--- main.js
+++ main.js
@@ -1,92 +1,90 @@
-function onClick(id){
-    const one = document.getElementById("one")
-    const two = document.getElementById("two")
-    const randolph = document.getElementById("randolph")
-    const textBox = document.getElementById("textBox")
-    const text = document.getElementById("text")
-    const clickSound = new Audio("sound/close.wav")
-    const typingSound = new Audio("sound/typing.wav")
-    const speed = 45;
-    const songs =[
-        new Audio("sound/billie-holiday.wav"),
-        new Audio("sound/bing2.wav"),
-        new Audio("sound/bing-crosby.wav"),
-        new Audio("sound/christmas.wav"),
-        new Audio("sound/cole-porter.wav"),
-        new Audio("sound/ella-fitz.wav"),
-        new Audio("sound/evelyn-knight.wav"),
-        new Audio("sound/frank-sinatra.wav"),
-        new Audio("sound/helen-forrest.wav"),
-        new Audio("sound/ink-spots.wav"),
-        new Audio("sound/kick-in-the-head.wav"),
-        new Audio("sound/peggy.wav"),
-        new Audio("sound/personality.wav"),
-        new Audio("sound/skeeter-davis.wav"),
-        new Audio("sound/three-suns.wav"),
-    ]
+const randolph = document.getElementById("randolph")
+const radio = document.getElementById("radio")
 
-    let song_counter = 0;
-    songs[song_counter].volume = 0.5
-    typingSound.loop = true
-    one.disabled = true
-    two.disabled = true
+const clickSound = new Audio("sound/close.wav")
+const typingSound = new Audio("sound/typing.wav")
+const speed = 20;
+let value = 0;
+typingSound.loop = true;
 
-    if(id === "one") {
-        if (one.innerHTML === "Thanks."){
+function onRandolph(id){
+    console.log("randolph")
+    clickSound.play()
+    document.getElementById(id).disabled = true;
+    document.getElementById("textBox").style.opacity = "100%";
+    let i = 0;
+    const txt = 'Welcome back. Want to listen to some tunes?';
+
+    function typeWriter() {
+        typingSound.play()
+        if (i < txt.length) {
+            document.getElementById("text").innerHTML += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+        else{
+            typingSound.pause()
+            typingSound.currentTime = 0;
+            one.disabled = false;
+        }
+    }
+    typeWriter()
+}
+
+function onDialogueBtns(id) {
+    const one = document.getElementById("one");
+    const two = document.getElementById("two");
+    const textBox = document.getElementById("textBox");
+    const text = document.getElementById("text");
+
+
+
+    // If the user presses the first choice button
+    if (id === "one") {
+        console.log("one")
+        // If the label says the closing message
+        if (one.innerHTML === "Thanks.") {
             textBox.style.opacity = 0;
             clickSound.play()
         }
-        else{
+        // For all other labels
+        else {
             text.innerHTML = '';
             const t = "Great choice. You can use that radio over there to change the channel if you're not loving the song.";
+
             let i = 0;
             typingSound.play()
-            songs[song_counter].play()
+
             function typeWriter() {
                 if (i < t.length) {
                     document.getElementById("text").innerHTML += t.charAt(i);
                     i++;
                     setTimeout(typeWriter, speed);
-                }
-                else{
+                } else {
                     typingSound.pause()
                 }
             }
+
             typeWriter()
 
             one.innerHTML = "Thanks."
             one.disabled = false;
+            radio.disabled = false
         }
     }
+}
 
-    else if (id === "randolph"){
-        clickSound.play()
-        randolph.disabled = true;
-        textBox.style.opacity = "100%";
-        let i = 0;
-        const txt = 'Welcome back. Want to listen to some tunes?';
-
-        function typeWriter() {
-            typingSound.play()
-            if (i < txt.length) {
-                document.getElementById("text").innerHTML += txt.charAt(i);
-                i++;
-                setTimeout(typeWriter, speed);
-            }
-            else{
-                typingSound.pause()
-                typingSound.currentTime = 0;
-                one.disabled = false;
-            }
-        }
-        typeWriter()
+function onRadio(id){
+    console.log(value)
+    let audio = document.getElementById(String(value));
+    if(audio.paused){
+        audio.play();
     }
+    else{
+        audio.pause();
+        value += 1;
+        audio = document.getElementById(String(value));
+        audio.play();
 
-    else if (id === "radio"){
-        // change the song here
-        songs[song_counter].pause();
-        song_counter++;
-        songs[song_counter].play();
     }
-
 }
